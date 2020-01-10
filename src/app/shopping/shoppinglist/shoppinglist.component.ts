158eb9642d519f6b29e30a6032b2091a5a767d32
Cloidt <18284831+Cloidt@users.noreply.github.com>
2020-01-10 17:27:48 +0100

    angularupdate Versuche

--- src/app/shopping/shoppinglist/shoppinglist.component.ts
+++ src/app/shopping/shoppinglist/shoppinglist.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
 import {ShoppingListService} from './shoppingList.service';
-import {Subscription} from 'rxjs/Subscription';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-shoppinglist',
